8048bc3e0e743ef6c7b78204f9751661a205bc4a
YuLinzhong <1427838626@qq.com>
2023-09-01 12:42:21 +0800

    Small Ball Movement Case v2.0

--- src/com/xcr/event_/BallMove.java
+++ src/com/xcr/event_/BallMove.java
@@ -20,34 +20,51 @@ public class BallMove extends JFrame {
     public BallMove() throws HeadlessException {
         mp = new MyPanel();
         this.add(mp);
-        this.setSize(400,300);
+        this.setSize(400, 300);
         //窗口JFrame对象可以监听键盘事件，即可以监听到面板发生的键盘事件
         this.addKeyListener(mp);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 }
+
 //KeyListener是监听器
 class MyPanel extends JPanel implements KeyListener {
+    int x = 10;
+    int y = 10;
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.fillOval(10, 10, 20, 20);
+        g.fillOval(x, y, 20, 20);
 
     }
+
     //有字符输出时，该方法触发
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
+
     //当某个键按下，该方法触发
     @Override
     public void keyPressed(KeyEvent e) {
-
+        //在java中会给每个键有一个值int
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {//向下的箭头对应的code值
+            y++;//更新坐标
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            y--;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            x++;
+        }else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+            x--;
+        }
+        this.repaint();
     }
+
     //当某个键松开，该方法触发
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println((char)e.getKeyCode()+"被按下");
+        //System.out.println((char) e.getKeyCode() + "被按下");
     }
 }
